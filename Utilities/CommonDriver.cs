51de137d37a33bb1624007c9da2a15eed6d195de
Amy Sara Baby <amysarababy77@gmail.com>
2025-03-05 11:03:43 +1300

    Implement Onboarding feature: Add tests for language and skills functionalities

---
+++ Utilities/CommonDriver.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SpecFlowProjectMars.Utilities
+{
+    public class CommonDriver
+    {
+        public static IWebDriver driver;
+        public void BrowserSetup()
+        {
+            driver= new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+         
+
+        }
+       
+        [TearDown]
+        public void CloseTestrun()
+        {
+            driver.Quit();
+        }
+    }
+}
